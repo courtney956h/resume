440a61e685f08adc37456e1dcf76d6dc1d16891c
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-12-06 20:12:59 +0900

    footer nav 스크롤 수정

--- js/common.js
+++ js/common.js
@@ -17,9 +17,36 @@
     })
 
     // nav 클릭시 current 유지
-    $('#footer .nav li a').on('click', function() {
-        $('#footer .nav li a.current').removeClass('current')
-        $(this).addClass('current')
+    $('#footer .nav li a').on('click', function(e) {
+        var ind = $(this).parent().index()
+        var wh = $(window).height()
+        
+        e.preventDefault()
+        $(this).parent().siblings().removeClass('current')
+        $(this).parent().addClass('current')
+        $('html, body').stop().animate({
+            scrollDown : ind*wh
+        },800)
     })
 
+
+    $('.section').on('mousewheel', function(e, wh) {
+        if ( wh>0 ) {
+            secIndex = $(this).prev().index()
+            if ( secIndex < 0 ) {
+                secIndex = 0
+            }
+        } else if ( wh<0 ) {
+            secIndex = $(this).next().index()
+            if ( secIndex < 0 ) {
+                secIndex = 4
+            }
+        }
+
+        $('html, body').stop().animate({
+            scrollDown : secIndex*$(window).height()
+        }, 800)
+        $('#footer .nav li').eq(secIndex).addClass('current')
+        $('#footer .nav li').eq(secIndex).siblings().removeClass('current')
+    })
 })(jQuery)
